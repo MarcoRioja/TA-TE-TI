e332870e5aa56a431ec997d9265936ee769b3b30
Marco <115066181+MarcoRioja@users.noreply.github.com>
2023-05-10 22:24:46 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+const infoBtn = document.getElementById('info');
+
+infoBtn.addEventListener('click', () => {
+	alert(`Nombre del alumno: Marco Emilio Rioja Carrasco
+	Curso: 1º DAM
+	Tema elegido de Proyecto Final: Página Web Asador`);
+});
